221183536ef806f06905f0e1be21dcd94c62f035
Ankur Agarwal <agarwalankur58@gmail.com>
2020-04-18 13:55:35 +0530

    refactor code

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { check, validationResult } = require('express-validator/check')
+const {check, validationResult} = require('express-validator/check')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
@@ -7,156 +7,156 @@ const router = express.Router() // eslint-disable-line new-cap
 
 const auth = require('../middleware/auth')
 
-let UserModel
+module.exports = ({user}) => {
+  const UserModel = user
 
-router.post(
-	'/signup',
-	[
-		check('email', 'Please enter a valid email').isEmail(),
-		check('password', 'Please enter a valid password').isLength({
-			min: 6
-		})
-	],
-	async (req, res) => {
-		const errors = validationResult(req)
-		if (!errors.isEmpty()) {
-			return res.status(400).json({
-				errors: errors.array()
-			})
-		}
+  router.post(
+    '/signup',
+    [
+      check('email', 'Please enter a valid email').isEmail(),
+      check('password', 'Please enter a valid password').isLength({
+        min: 6
+      })
+    ],
+    async (req, res) => {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        })
+      }
 
-		const { password, email } = req.body
+      const {password, email} = req.body
 
-		try {
-			let user = await UserModel.findOne({ email })
+      try {
+        let user = await UserModel.findOne({email})
 
-			console.table(user)
-			if (user) {
-				return res.status(400).json({
-					msg: 'User Already Exists'
-				})
-			}
+        console.table(user)
+        if (user) {
+          return res.status(400).json({
+            msg: 'User Already Exists'
+          })
+        }
 
-			user = new UserModel({
-				email,
-				password
-			})
+        user = new UserModel({
+          email,
+          password
+        })
 
-			const salt = await bcrypt.genSalt(10)
-			user.password = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
 
-			await user.save()
+        await user.save()
 
-			const payload = {
-				user: {
-					id: user.id
-				}
-			}
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
 
-			jwt.sign(
-				payload,
-				'randomString',
-				{
-					expiresIn: 10000
-				},
-				(err, token) => {
-					if (err) {
-						throw err
-					}
+        jwt.sign(
+          payload,
+          'randomString',
+          {
+            expiresIn: 10000
+          },
+          (err, token) => {
+            if (err) {
+              throw err
+            }
 
-					res.status(200).json({
-						token
-					})
-				}
-			)
-		} catch (error) {
-			console.log(error.message)
-			res.status(500).send('Error while saving user')
-		}
-	})
+            res.status(200).json({
+              token
+            })
+          }
+        )
+      } catch (error) {
+        console.log(error.message)
+        res.status(500).send('Error while saving user')
+      }
+    })
 
-router.post(
-	'/login',
-	[
-		check('email', 'Please enter a valid email').isEmail(),
-		check('password', 'Please enter a valid password').isLength({
-			min: 6
-		})
-	],
-	async (req, res) => {
-		const errors = validationResult(req)
+  router.post(
+    '/login',
+    [
+      check('email', 'Please enter a valid email').isEmail(),
+      check('password', 'Please enter a valid password').isLength({
+        min: 6
+      })
+    ],
+    async (req, res) => {
+      const errors = validationResult(req)
 
-		if (!errors.isEmpty()) {
-			return res.status(400).json({
-				errors: errors.array()
-			})
-		}
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        })
+      }
 
-		const { email, password } = req.body
-		try {
-			const user = await UserModel.findOne({
-				email
-			})
-			if (!user) {
-				return res.status(400).json({
-					message: 'User Not Exist'
-				})
-			}
+      const {email, password} = req.body
+      try {
+        const user = await UserModel.findOne({
+          email
+        })
+        if (!user) {
+          return res.status(400).json({
+            message: 'User Not Exist'
+          })
+        }
 
-			const isMatch = await bcrypt.compare(password, user.password)
-			if (!isMatch) {
-				return res.status(400).json({
-					message: 'Incorrect Password !'
-				})
-			}
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+          return res.status(400).json({
+            message: 'Incorrect Password !'
+          })
+        }
 
-			const payload = {
-				user: {
-					id: user.id
-				}
-			}
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
 
-			jwt.sign(
-				payload,
-				'secret',
-				{
-					expiresIn: 3600
-				},
-				(err, token) => {
-					if (err) {
-						throw err
-					}
+        jwt.sign(
+          payload,
+          'secret',
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) {
+              throw err
+            }
 
-					res.status(200).json({
-						token
-					})
-				}
-			)
-		} catch (error) {
-			console.error(error)
-			res.status(500).json({
-				message: 'Server Error'
-			})
-		}
-	}
-)
+            res.status(200).json({
+              token
+            })
+          }
+        )
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({
+          message: 'Server Error'
+        })
+      }
+    }
+  )
 
-router.get('/me', auth, async (req, res) => {
-	try {
-		// Request.user is getting fetched from Middleware after token authentication
-		const user = await UserModel.findById(req.user.id)
-		res.json(user)
-	} catch (error) {
-		if (error) {
-			console.log(error)
-			res.status(500).send('Error while fetching details!')
-		}
+  router.get('/me', auth, async (req, res) => {
+    try {
+      // Request.user is getting fetched from Middleware after token authentication
+      const user = await UserModel.findById(req.user.id)
+      res.json(user)
+    } catch (error) {
+      if (error) {
+        console.log(error)
+        res.status(500).send('Error while fetching details!')
+      }
 
-		res.send({ message: 'Error in Fetching user' })
-	}
-})
+      res.send({message: 'Error in Fetching user'})
+    }
+  })
 
-module.exports = ({ user }) => {
-	UserModel = user
-	return router
+
+  return router
 }
